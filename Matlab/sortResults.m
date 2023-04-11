1ddcfe4d74d6b405e3bb1b1ea03ba8c20a51d5d9
Tyler <47286889+tapevans@users.noreply.github.com>
2023-04-11 12:03:59 -0600

    Update sortResults.m
    Cleaned up this file. Need to split out subscores to its own file.

--- Matlab/sortResults.m
+++ Matlab/sortResults.m
@@ -1,17 +1,27 @@
 
 function [Atlantic, Metro , Central, Pacific, subScores] = sortResults(Teams,Scores,IDX)
+%% Separate into divisons
 T = struct2table(Teams);
 sortedT = sortrows(T , 'divison');
+
 Atlantic = sortedT(1:8,:);
+Central  = sortedT(9:16,:);
+Metro    = sortedT(17:24,:);
+Pacific  = sortedT(25:32,:);
+
+
+%% Sort each division by Points
 Atlantic = sortrows(Atlantic,'Pts', 'descend');
-Central = sortedT(9:16,:);
-Central = sortrows(Central,'Pts', 'descend');
-Metro = sortedT(17:24,:);
-Metro = sortrows(Metro,'Pts', 'descend');
-Pacific = sortedT(25:32,:);
-Pacific = sortrows(Pacific,'Pts', 'descend');
+Central  = sortrows(Central ,'Pts', 'descend');
+Metro    = sortrows(Metro   ,'Pts', 'descend');
+Pacific  = sortrows(Pacific ,'Pts', 'descend');
 
 
+%% Determine current divison rank
+% Split tie breakers
+
+
+%% !!!!!!!!!Split off subscores into its own function
 TT = 6;
 subScores = [Scores(IDX(TT).GameIDXAway);Scores(IDX(TT).GameIDXHome)];
 
