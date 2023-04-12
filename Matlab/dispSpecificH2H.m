44696833ba10fe1489b929d24510ecbd04cb5961
Tyler <47286889+tapevans@users.noreply.github.com>
2023-04-12 12:03:08 -0600

    Updated viewing order

---
+++ Matlab/dispSpecificH2H.m
@@ -0,0 +1,6 @@
+function dispSpecificH2H(desiredTeamIDX , opposingTeamIDX , Teams, H2H)
+    desired_str  = Teams(desiredTeamIDX).name;
+    opposing_str = Teams(opposingTeamIDX).name;
+    record = H2H{desiredTeamIDX , opposingTeamIDX};
+    disp([desired_str ' have a record of ' num2str(record(1)) '-' num2str(record(2)) '-' num2str(record(3)) ' against the ' opposing_str])
+end
--- Matlab/sortResults.m
+++ Matlab/sortResults.m
@@ -1,5 +1,5 @@
 
-function [Atlantic, Metro , Central, Pacific, subScores] = sortResults(Teams,Scores,IDX)
+function [Atlantic, Metro , Central, Pacific, Eastern, Western, subScores] = sortResults(Teams,Scores,IDX)
 %% Separate into divisons
 T = struct2table(Teams);
 sortedT = sortrows(T , 'divison');
@@ -9,20 +9,31 @@ Central  = sortedT(9:16,:);
 Metro    = sortedT(17:24,:);
 Pacific  = sortedT(25:32,:);
 
+%% Separate into Conference
+T = struct2table(Teams);
+sortedT = sortrows(T , 'conference');
+
+Eastern = sortedT(1 :16,:);
+Western = sortedT(17:32,:);
 
 %% Sort each division by Points
-Atlantic = sortrows(Atlantic,'Pts', 'descend');
-Central  = sortrows(Central ,'Pts', 'descend');
-Metro    = sortrows(Metro   ,'Pts', 'descend');
-Pacific  = sortrows(Pacific ,'Pts', 'descend');
+% Atlantic = sortrows(Atlantic,'Pts', 'descend');
+% Central  = sortrows(Central ,'Pts', 'descend');
+% Metro    = sortrows(Metro   ,'Pts', 'descend');
+% Pacific  = sortrows(Pacific ,'Pts', 'descend');
 
+Atlantic = sortrows(Atlantic,'DivRank');
+Central  = sortrows(Central ,'DivRank');
+Metro    = sortrows(Metro   ,'DivRank');
+Pacific  = sortrows(Pacific ,'DivRank');
 
-%% Determine current divison rank
-% Split tie breakers
+%% Sort each Conference by Points
+Eastern = sortrows(Eastern,'Pts', 'descend');
+Western  = sortrows(Western ,'Pts', 'descend');
 
 
 %% !!!!!!!!!Split off subscores into its own function
-TT = 6;
+TT = 8;
 subScores = [Scores(IDX(TT).GameIDXAway);Scores(IDX(TT).GameIDXHome)];
 
 % N_teams     = length(Teams);
